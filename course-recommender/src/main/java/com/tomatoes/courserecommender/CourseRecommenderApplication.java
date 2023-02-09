7775b975a6f1cdf626edbf922a7ebbb7dc5c28d6
mady-dx <msarmiento1621@gmail.com>
2023-02-08 20:02:56 -0800

    Initial commit for assignment 3

---
+++ course-recommender/src/main/java/com/tomatoes/courserecommender/CourseRecommenderApplication.java
@@ -0,0 +1,14 @@
+package com.tomatoes.courserecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRecommenderApplication.class, args);
+		System.out.println("Hello World :)");
+	}
+
+}
---
+++ course-recommender/src/test/java/com/tomatoes/courserecommender/CourseRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomatoes.courserecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
