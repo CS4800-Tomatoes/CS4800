eb57784cf9be272063a028c2f368b42b756cb224
ConanJian <conanjian2@gmail.com>
2023-04-26 19:32:17 -0700

    Updated Class Model

--- course-recommender/src/main/java/com/cpp/tomatoes/courserecommender/Models/Class.java
+++ course-recommender/src/main/java/com/cpp/tomatoes/courserecommender/Models/Class.java
@@ -20,13 +20,18 @@ public class Class {
     @SerializedName("Tag")
     private int[] _tagList;
 
+    @SerializedName("Image")
+    private String _imageUrl;
+
     public void setCourseNumber(int num){_courseNumber = num;}
     public void setClassName(String name){_className = name;}
     public void setDescription(String description){_description = description;}
     public void setTagList(int[] tagList){_tagList = tagList;}
+    public void setImageUrl(String url){_imageUrl = url;}
 
     public int getCourseNum(){return _courseNumber;}
     public String getClassName(){return _className;}
     public String getDescription(){return _description;}
     public int[] getTagList(){return _tagList.clone();}
+    public String getImageUrl(){return _imageUrl;}
 }
